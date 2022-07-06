d313f0bd11fc2bedf5316efb604a3d3d9102e753
Justin <jlmyers1993@hotmail.com>
2022-07-06 11:10:56 -0600

    commit tree 4c91246919ee3d495cbbc14f80e7603cbd485c41

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+// function getBooks() {}
+const getBooks = () => {
+    fetch('http://localhost:3000/books')
+    .then(res => res.json())
+    .then(json => displayTitlesForBooks(json))
+}
+
+const displayTitlesForBooks = books => {
+    const list = document.getElementById('list');
+    books.forEach(book => { 
+        const item = document.createElement('li');
+        item.textContent = book.title;
+        addListenerToTitle(item);
+        list.append(item);
+    })
+}
+
+const addListenerToTitle = (listItem, book) => {
+    const showPanel = codument.getElementById('show-panel');
+    listItem.addEventListener('click', () => {
+        showPanel.textContent = "";
+        const image = document.createElement('img');
+        image.src = book.img_url;
+        const p = document.createElement('p');
+        p.textContent = book.description;
+
+        showPanel.append(image, p);
+    })
+}
+
+getBooks();
